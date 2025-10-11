a237012d44aed08037c4397cc9d3807845b0fbe1
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2025-10-12 00:16:25 +0530

    StackGuard Auto-Remediation: Secure secrets in server.py

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
-
 from flask import Flask, request, jsonify
 import google.generativeai as genai
+import os
+import hvac
 
 app = Flask(__name__)
 
+client = hvac.Client(
+    url=os.environ['VAULT_ADDR'],
+    token=os.environ['VAULT_TOKEN']
+)
+read_secret_result = client.secrets.kv.v2.read_secret_version(path='chethanreddy123-python-test-server-server-py-69437cd3')
+
 # Gemini API key (EXPOSED FOR TESTING PURPOSES)
-GEMINI_API_KEY = "AIzaSyDdXdbW1BxCg8N5YRxpbm3cq_uSWL14IvA"
+GEMINI_API_KEY = read_secret_result['data']['data']['secret']
 MODEL_NAME = "gemini-2.5-flash"
 
 genai.configure(api_key=GEMINI_API_KEY)
